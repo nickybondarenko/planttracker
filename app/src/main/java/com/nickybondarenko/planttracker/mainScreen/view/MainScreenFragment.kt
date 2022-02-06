b963fb34c958fe5e4eeb387dc6b0464a0ae70ad4
Nicky Bondarenko <nicky@door2door.io>
2022-02-06 19:23:15 +0100

    Add mainScreen fragment

---
+++ app/src/main/java/com/nickybondarenko/planttracker/mainScreen/view/MainScreenFragment.kt
@@ -0,0 +1,7 @@
+package com.nickybondarenko.planttracker.mainScreen.view
+
+import androidx.fragment.app.Fragment
+import com.nickybondarenko.planttracker.R
+
+class MainScreenFragment: Fragment(R.layout.main_screen_fragment) {
+}
