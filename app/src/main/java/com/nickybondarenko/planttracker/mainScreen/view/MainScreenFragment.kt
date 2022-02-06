37f2d4afcb1b84e5963cc45c3d03051af40a9cfc
Nicky Bondarenko <nicky@door2door.io>
2022-02-06 19:44:31 +0100

    Add plant item fragment

--- app/src/main/java/com/nickybondarenko/planttracker/mainScreen/view/MainScreenFragment.kt
+++
@@ -1,7 +0,0 @@
-package com.nickybondarenko.planttracker.mainScreen.view
-
-import androidx.fragment.app.Fragment
-import com.nickybondarenko.planttracker.R
-
-class MainScreenFragment: Fragment(R.layout.main_screen_fragment) {
-}
